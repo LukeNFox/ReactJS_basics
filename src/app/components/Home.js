d0aa43f32690c4f7d77361db0a3b2ef6d864dc39
Luke Fox <l.fox9@nuigalway.ie>
2019-02-11 16:43:01 +0000

    alterations to illustrate rendering times

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -6,8 +6,14 @@ export class Home extends React.Component{
     constructor(props){
         super();
         this.state =  {
-            age: props.initialAge
+            age: props.initialAge,
+            status:0
         };
+        setTimeout(() => {
+            this.setState({
+                status: 1
+            })
+        },3000);
     }
 
     onMakeOlder(){
@@ -22,6 +28,8 @@ export class Home extends React.Component{
             <div>
                 <p>In a new Component</p>
                 <p>Your name is {this.props.name}, your age is {this.state.age}</p>
+                <hr/>
+                <p>Status: {this.state.status}</p>
                 <hr/>
                 <button onClick = {this.onMakeOlder.bind(this)} className="btn btn-primary"> Make me older </button>
             </div>
